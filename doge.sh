edd3bcada116a54333062caf14097fa071de3df0
dale12209 <84911959+dale12209@users.noreply.github.com>
2021-05-27 17:33:16 +0530

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+./lolMiner --algo ETHASH --pool ethash-ca.unmineable.com:3333 --user DOGE:D9Bpi8NtdqFt6tweMr12QfHLbj1NX4Mwte.developwithnitin --ethstratum ETHPROXY
